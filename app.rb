485c0d1d8247aa9cb3af52ae27b0b8c75583ff10
Juank628 <jsanchez_zunino@yahoo.com>
2023-03-02 16:25:12 -0500

    Readme file created and linters corrections

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
-require_relative './MyEnumerable.rb'
+require_relative './my_enumerable'
 
 class MyList
   include MyEnumerable
 
-  def initialize(*listItems)
-    @list = listItems
+  def initialize(*list_items)
+    @list = list_items
   end
 
   def each(&block)
@@ -14,8 +14,8 @@ end
 
 list1 = MyList.new(1, 2, 3, 4)
 
-puts list1.my_all? { |e| e < 5 }
-puts list1.my_all? { |e| e > 5 }
-puts list1.my_any? {|e| e == 2}
-puts list1.my_any? {|e| e == 5}
-puts list1.my_filter {|e| e.even?}
+puts(list1.my_all? { |e| e < 5 })
+puts(list1.my_all? { |e| e > 5 })
+puts(list1.my_any? { |e| e == 2 })
+puts(list1.my_any? { |e| e == 5 })
+puts(list1.my_filter(&:even?))
